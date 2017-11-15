3a65af0a524bbd587ab99e2916b65dc47a2cc101
joel1524 <macewandu@hotmail.com>
2017-11-15 14:07:50 -0500

    WIP

--- AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/HomeActivity.java
+++ AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/HomeActivity.java
@@ -1,7 +1,16 @@
 package cashflow.getmoney.amazeme;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +18,39 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Adds toolbar to activity
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        Drawable settings = ContextCompat.getDrawable(this, R.drawable.ic_settings);
+        mToolbar.setNavigationIcon(settings);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("HOME", "CLICKED ON SETTINGS");
+                Intent intent = new Intent(HomeActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.home_actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_friends:
+                Intent intent = new Intent(this, FriendsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/SignUpActivity.java
+++ AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/SignUpActivity.java
@@ -2,9 +2,11 @@ package cashflow.getmoney.amazeme;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,6 +31,12 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
         newUsername = (EditText) findViewById(R.id.newUsername);
         newPassword = (EditText) findViewById(R.id.newPassword);
