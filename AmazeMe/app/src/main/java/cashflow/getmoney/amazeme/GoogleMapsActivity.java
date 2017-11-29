9348f56b651e10596321ca6716cfd9492f3d92a8
joel1524 <macewandu@hotmail.com>
2017-11-29 17:57:41 -0500

    WIP

--- AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/GoogleMapsActivity.java
+++ AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/GoogleMapsActivity.java
@@ -1,68 +1,214 @@
 package cashflow.getmoney.amazeme;
 
-import android.*;
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-<<<<<<< HEAD
+
 import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
-=======
->>>>>>> origin/master
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationAvailability;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCallback,
+GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+GoogleMap.OnMarkerClickListener, LocationListener {
 
     private GoogleMap mMap;
-<<<<<<< HEAD
-    LatLng mCoordinates;
-    LocationManager locationManager;
-    MarkerOptions mo;
-    Marker marker;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private boolean mLocationUpdateState;
 
-    final int FINE_LOCATION_REQUEST_CODE = 0;
-=======
-    int FINE_LOCATION_REQUEST_CODE = 0;
->>>>>>> origin/master
+    private static final int FINE_LOCATION_REQUEST_CODE = 1;
+    private static final int REQUEST_CHECK_SETTINGS = 2;
 
+    protected void startLocationUpdates() {
+        // If the ACCESS_FINE_LOCATION permission has not been granted, request it now and return
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+            return;
+        }
+        // If there is permission, request for location updates
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
 
+    // Create instance of LocationRequest, add it to instance of LocationSettingsRequest.Builder and retrieve
+    // and handle changes to be made based on current state of user's location settings.
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        // Specifies the rate at which app will like to receive updates
+        mLocationRequest.setInterval(10000);
+        // Specifies the fastest rate at which the app can handle updates
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(@NonNull LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch(status.getStatusCode()) {
+                    // A SUCCESS means location request can be initialized
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        mLocationUpdateState = true;
+                        startLocationUpdates();
+                        break;
+                    // A RESOLUTION_REQUIRED means location settings have some issues which can be fixed.
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            status.startResolutionForResult(GoogleMapsActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+
+                        }
+                        break;
+                    // A SETTINGS_CHANGE_UNAVAILABLE means the location settings have some issues that you can't fix
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        break;
+                }
+            }
+        });
+    }
+
+    private void setUpMap() {
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+            return;
+        }
+
+        // enables the my location layer which draws a light blue dot on the user's location
+        // also adds button to center map on user's location
+        mMap.setMyLocationEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // determines the availability of location data on the device
+        LocationAvailability locationAvailability = LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient);
+        if(null != locationAvailability && locationAvailability.isLocationAvailable()) {
+            // gives the most recent location currently available
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            // If most recent location was successfully retrieved, move camera to user's current location
+            if(mLastLocation != null) {
+                LatLng currentLocation = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+                // add pin at user's location
+                placeMarkerOnMap(currentLocation);
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 18));
+            }
+        }
+    }
+
+    protected void placeMarkerOnMap(LatLng location) {
+        // Create MarkerOptions object and set the user's current location as the position for the marker
+        MarkerOptions markerOptions = new MarkerOptions().position(location);
+        // Add marker to map
+        mMap.addMarker(markerOptions);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
 
+        if(mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        mo = new MarkerOptions().position(new LatLng(0, 0)).title("Current Location");
+        createLocationRequest();
+    }
+
+    @Override
+    protected void onStart() {
+        super .onStart();
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super .onStop();
+        if(mGoogleApiClient != null && mGoogleApiClient.isConnected() ) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    // Start the update request if it has a RESULT_OK result for a REQUEST_CHECK_SETTINGS request
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super .onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CHECK_SETTINGS) {
+            if(resultCode == RESULT_OK) {
+                mLocationUpdateState = true;
+                startLocationUpdates();
+            }
+        }
+    }
+
+    // Stop location update request
+    @Override
+    protected void onPause() {
+        super .onPause();
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    // Restart location update request
+    @Override
+    public void onResume() {
+        super .onResume();
+        if(mGoogleApiClient.isConnected() && !mLocationUpdateState) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        setUpMap();
+
+        if(mLocationUpdateState) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
     }
 
 
@@ -78,95 +224,76 @@ public class GoogleMapsActivity extends FragmentActivity implements OnMapReadyCa
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
         // Initialize Google Play Services
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-<<<<<<< HEAD
-=======
 
-
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-
->>>>>>> origin/master
             // Should we show an explanation?
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response!
+                AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setCancelable(false);
+                dialog.setTitle("Permission access")
+                        .setMessage("This app uses your location in order to generate the maze. Please allow access.")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(GoogleMapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+                            }
+                        });
+                dialog.show();
 
             } else {
                 // No explanation needed, we can request the permission
-                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_REQUEST_CODE);
             }
-
             return;
         } else {
             mMap.setMyLocationEnabled(true);
-            // Set a preference for minimum and maximum zoom
-            mMap.setMinZoomPreference(15.0f);
-            mMap.setMaxZoomPreference(20.0f);
         }
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
 
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
-
-    }
-
-    private void requestLocation() {
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setPowerRequirement(Criteria.POWER_HIGH);
-        String provider = locationManager.getBestProvider(criteria, true);
-        locationManager.requestLocationUpdates(provider, 10000, 10, this);
-    }
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case FINE_LOCATION_REQUEST_CODE: {
                 // If request is cancelled, the result arrays are empty.
-                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // Permission was granted! Do the location related
                     // task that needs to be done
-<<<<<<< HEAD
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                        mMap.setMyLocationEnabled(true);
-                        // Set a preference for minimum and maximum zoom
-                        mMap.setMinZoomPreference(15.0f);
-                        mMap.setMaxZoomPreference(20.0f);
-
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
                     }
-=======
                     mMap.setMyLocationEnabled(true);
->>>>>>> origin/master
                 } else {
                     // Permission denied! Disable the functionality
                     // that depends on this permission
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show();
-
                 }
                 return;
             }
         }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        if(null != mLastLocation) {
+            mMap.clear();
+            placeMarkerOnMap(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+        }
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 }
