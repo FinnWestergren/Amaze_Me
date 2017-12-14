eada54e161d43bf25acc93de0fbcda51c9e95198
hlshock <hlshock@gmail.com>
2017-12-14 00:26:36 -0500

    Maze Object

---
+++ AmazeMe/app/src/main/java/cashflow/getmoney/amazeme/Maze.java
@@ -0,0 +1,30 @@
+package cashflow.getmoney.amazeme;
+
+/**
+ * Created by hlsho_000 on 12/13/2017.
+ */
+
+public class Maze {
+    private int index;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(String completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    private String completionDate;
+    public Maze(){
+        
+    }
+}
